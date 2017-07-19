2b9c16ace19a24b32e4c286cd7bf09f42e326921
AcerFeng <qinlanfeng@foxmail.com>
2017-07-19 17:39:17 +0800

    新增：高德地图官方 Demo

---
+++ Demo/地图/高德地图官方Demo/MAMapKit_2D_Demo/MAMapKit_2D_Demo/Configuration/APIKey.h
@@ -0,0 +1,17 @@
+//
+//  APIKey.h
+//  SearchV3Demo
+//
+//  Created by songjian on 13-8-14.
+//  Copyright (c) 2013年 songjian. All rights reserved.
+//
+
+#ifndef SearchV3Demo_APIKey_h
+#define SearchV3Demo_APIKey_h
+
+/* 使用高德地图API，请注册Key，注册地址：http://lbs.amap.com/dev/#/ */
+
+const static NSString *APIKey = @"ea92507701e193395e9a2799252bf020";
+const static NSString *TableID = @"";
+
+#endif
